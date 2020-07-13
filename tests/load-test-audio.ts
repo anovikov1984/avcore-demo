1efb1fdedc904297cf434c6eb54bfabf410f9301
Alexander Kiyashko <angedonik@gmail.com>
2020-07-13 16:29:34 +0300

    demo

--- tests/load-test-audio.ts
+++ tests/load-test-audio.ts
@@ -37,12 +37,18 @@ export async function loadTestAudio() {
         page.on('pageerror', onError);
         page.on('error', onError);
         page.on('load', async () => {
-                setTimeout(async () => {
+            const click = async () => {
+                try {
                     await page.click('#subscribe');
-                }, 1000);
-                resolve();
-            }
-        );
+                    resolve()
+                }
+                catch (e) {
+                    setTimeout(click, 1000);
+
+                }
+            };
+            await click();
+        });
         await page.goto('https://avcore-demo.codeda.com/demoLoadTest.html?originUrl=https://meeting-mcu.codeda.com:8080&worker=random&stream=09beafb1-32e2-437a-87b0-e08d07ba66f8&token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdHJlYW0iOiIwOWJlYWZiMS0zMmUyLTQzN2EtODdiMC1lMDhkMDdiYTY2ZjgiLCJvcGVyYXRpb24iOiIxIiwiaWF0IjoxNTk0NjA5MjAwfQ.YHPtobQhdiH6O9I2QsBshkDO-aLWdkuXT00GtQo_m2uZsYQm0tFrDj44d-nqOx-OHLzUv_KUlzQ03DWGYbtOzw&kinds=audio&num=50');
 
     })
